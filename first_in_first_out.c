7d951c37938c75fc47fb60b1c35aaaa4d4671cce
Lemuel Jay Enad <92448937+lemuelenad@users.noreply.github.com>
2022-11-02 16:28:57 +0800

    Update first_in_first_out.c

--- first_in_first_out.c
+++ first_in_first_out.c
@@ -3,49 +3,30 @@ Lemuel Jay B. Enad BSCPE 4A
 First IN First OUT
 */
 
-//Declaring variables
-int LED1 = 13;
-int LED2 = 12;
-int LED3 = 11;
-int LED4 = 10;
-int LED5 = 9;
-int LED6 = 8;
-
-//Setup pin outputs
-void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
-  pinMode(LED6, OUTPUT);
+int time[6] = {10000, 8000, 7000, 1000, 5000, 3000};
+int count = 0; //used to identifies the element of time array.
+  
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
-//LED loop
-void loop() {
-  digitalWrite(LED1, HIGH); // turn on LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED1, LOW);	// turn off LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED2, HIGH);	// turn on LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED2, LOW);	// turn off LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED3, HIGH);	// turn on LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED3, LOW);	// turn off LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED4, HIGH);	// turn on LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED4, LOW);	// turn off LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED5, HIGH);	// turn on LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED5, LOW);	// turn off LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED6, HIGH);	// turn on LED
-  delay(1000);				// 1s delay
-  digitalWrite(LED6, LOW);	// turn off LED
-  delay(1000);				// 1s delay
-  exit(0);			//terminate loop process
+void loop()
+{
+  first_come_first_serve();  //calling the function.
+  exit(0);
+}
+
+void first_come_first_serve(){
+  for (int i=13; i>=8; i--){
+  	digitalWrite(i, HIGH);	 //Turn the LED On
+    delay(time[count]); 	 //Delay.
+    digitalWrite(i, LOW);    //Turn the LED Off
+    count++;                 //Increment count.
+  }
 }
